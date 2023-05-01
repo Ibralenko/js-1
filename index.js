da77de909b65ae685458576c8672bc15deb335c3
[ibralenko6724809@gmail.com] <ibralenko6724809@gmail.com>
2023-05-01 22:10:23 +0300

    end

--- index.js
+++ index.js
@@ -3,3 +3,41 @@ let cleaner = 40000;
 let teacher = 22000;
 let result = cleaner+teacher;
 console.log(result);
+//2
+let city ='Владивосток'
+let number= 2000;
+let result2=(`${city} ${number}`);
+console.log(result2);
+//3
+let messenger = 128;
+let photo = 254;
+let vk = 137;
+let youtube=201;
+let minutes=messenger+photo+vk+youtube;
+let hours=minutes/60;
+console.log(hours);
+//4
+let x=2**5;
+console.log(x);
+//5
+let text='"Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!"'
+console.log(text);
+//6
+const greetings = "Привет";
+const name = "Кот";
+let result3=(`${greetings}, ${name}`);
+console.log(result3);
+//7
+let time = 34;
+console.log('Старт поездки. Осталось минут: ' + time);
+time=15;
+console.log('Немного сториз в соцсетях. Осталось минут: ' + time);
+time = 10;
+console.log('Немного не новостей, но событий. Осталось минут: ' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+//8
+let fahrenheit = 451;
+let celsius =(fahrenheit - 32) / 1.8;
+let result4=(`${fahrenheit} градуса по Фаренгейту - это ${celsius}градуса по Цельсию.`);
+console.log(result4);
